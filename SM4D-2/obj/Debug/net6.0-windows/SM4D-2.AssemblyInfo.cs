6537f65626128f70b2eecaf9f0bb861cfefd3f19
SamuhellCaron <carons.redacteur@gmail.com>
2023-12-29 14:28:59 -0500

    Test git ignore
    Commiter un changement au App.config devrait être ignoré. Donc le vieux nom du serveur devrait rester à Desktop...

--- SM4D-2/obj/Debug/net6.0-windows/SM4D-2.AssemblyInfo.cs
+++ SM4D-2/obj/Debug/net6.0-windows/SM4D-2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SM4D-2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+356b1cec1fc2d0263831e10d27bc7f6a69ec71b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SM4D-2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SM4D-2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
